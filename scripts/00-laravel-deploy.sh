bc637df88df09e64ee18263a28149c2c5ba9e423
sudrajadsasmita <sudrajad.sasmita@gmail.com>
2023-04-22 01:19:00 +0700

    test docker

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,4 +1,7 @@
 # !/usr/bin/env bash
+echo "Run Access"
+chmod -R 777 storage/
+
 echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
