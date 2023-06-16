e5207c15ccfd3331b60e913577cec27ca07eaaec
sudrajadsasmita <sudrajad.sasmita@gmail.com>
2023-06-16 14:13:57 +0700

    change validation register

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -38,13 +38,13 @@ class RegisterRequest extends FormRequest
     {
         return [
             "username" => "required|string|unique:users,username",
-            "email" => 'required|email|string',
             "password" => 'required|string|confirmed|min:6',
-            "nama" => "string|required",
             "prodi_id" => "integer|required",
-            "ipk" => "numeric|required",
-            "tahun_masuk" => "date_format:Y|required",
-            "tahun_lulus" => "date_format:Y|required",
+            "email" => 'nullable|email|string',
+            "nama" => "string|nullable",
+            "ipk" => "numeric|nullable",
+            "tahun_masuk" => "date_format:Y|nullable",
+            "tahun_lulus" => "date_format:Y|nullable",
         ];
     }
     public function messages(): array
@@ -53,23 +53,18 @@ class RegisterRequest extends FormRequest
             'username.required' => 'Isian kolom username tidak boleh kosong',
             'username.string' => 'Isian kolom username harus berupa teks',
             'username.unique' => 'Isian kolom username sudah terdaftar',
-            'email.required' => 'Isian kolom email tidak boleh kosong',
-            'email.string' => 'Isian kolom email harus berupa teks',
-            'email.email' => 'Isian kolom email harus sesuai dengan format email',
             'password.required' => 'Isian kolom password tidak boleh kosong',
             'password.string' => 'Isian kolom password harus berupa teks',
             'password.confirmed' => 'Isian kolom password tidak sama',
             'password.min' => 'Isian kolom password kurang dari 6 karakter',
-            'nama.string' => 'Isian dari kolom nama harus berupa teks',
-            'nama.required' => 'Isian dari kolom nama tidak boleh kosong',
             'prodi_id.required' => 'Isian dari kolom prodi tidak boleh kosong',
             'prodi_id.integer' => 'Isian dari kolom prodi harus berupa angka',
+            'email.string' => 'Isian kolom email harus berupa teks',
+            'email.email' => 'Isian kolom email harus sesuai dengan format email',
+            'nama.string' => 'Isian dari kolom nama harus berupa teks',
             'ipk.numeric' => 'Isian dari kolom ipk harus berupa bilangan',
-            'ipk.required' => 'Isian dari kolom ipk tidak boleh kosong',
             'tahun_masuk.date_format' => 'Isian dari kolom tahun masuk harus berupa tahun',
-            'tahun_masuk.required' => 'Isian dari kolom tahun masuk tidak boleh kosong',
             'tahun_lulus.date_format' => 'Isian dari kolom tahun lulus harus berupa tahun',
-            'tahun_lulus.required' => 'Isian dari kolom tahun lulus tidak boleh kosong',
         ];
     }
 }
